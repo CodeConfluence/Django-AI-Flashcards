a280dcfbc25ef7022d955d740a82a44276368a99
Kristan Seenath-Nagassar <snkris@umich.edu>
2024-08-18 15:51:13 -0300

    Updated project urls to support user payment app

--- flashcard_project/flashcard_project/urls.py
+++ flashcard_project/flashcard_project/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', include('flashcard_app.urls')),
+    path('', include('user_payment.urls')),
 ]
