c70bbd4772d668e5381140599c1f9ac5f1e9bfcf
Kristan Seenath-Nagassar <snkris@umich.edu>
2024-08-19 06:24:30 -0300

    Minor fixes

--- flashcard_project/flashcard_project/urls.py
+++ flashcard_project/flashcard_project/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', include('flashcard_app.urls')),
     path('', include('user_payment.urls')),
+    path('', include('AI.urls')),
 ]
